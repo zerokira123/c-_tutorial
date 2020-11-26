c13387d06757b2a3640c6112f461b6f276d92ecf
zerokira123 <thfication01@gmail.com>
2020-11-26 19:17:45 +0530

    seen till 27:24

--- giraffe/giraffe/Program.cs
+++ giraffe/giraffe/Program.cs
@@ -10,11 +10,14 @@ namespace giraffe
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ENTER YOUR NAME");
+            string chName = Console.ReadLine();
+            int intAge = 20;
 
-            Console.WriteLine("hello world");
-
-           string a  = Console.ReadLine();
-            Console.WriteLine(a);
+            Console.WriteLine("There was once a man named " + chName);
+            Console.WriteLine("He was " +intAge+ " Years old");
+            Console.WriteLine("he realy liked the Name " + chName);
+            Console.WriteLine("but he didn't like being " + intAge);
             Console.ReadLine();
 
         }
