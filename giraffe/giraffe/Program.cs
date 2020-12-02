e32ec6806f924230d5d1fd4ce849a0e4570ecbd0
zerokira123 <thfication01@gmail.com>
2020-12-02 20:55:53 +0530

    seen till 1:08

--- giraffe/giraffe/Program.cs
+++ giraffe/giraffe/Program.cs
@@ -10,9 +10,13 @@ namespace giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO");
+            Console.WriteLine("ENTER YOUR NAME");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("ENTER YOUR AGE");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " Your " + age);
 
             Console.ReadLine();
 
